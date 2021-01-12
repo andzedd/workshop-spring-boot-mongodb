f5012de08dbe6be73db99f0e6fdabc9fcd084625
Matheus dos Santos Souza <ssouza.matheus@outlook.com>
2021-01-12 17:08:08 -0300

    full query search

--- src/main/java/com/andzedd/workshopmongo/resources/PostResource.java
+++ src/main/java/com/andzedd/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.andzedd.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="min", defaultValue="") String min,
+			@RequestParam(value="max", defaultValue="") String max){
+		text = URL.decodeParam(text);
+		Date minDate = URL.convertDate(min, new Date(0L));
+		Date maxDate = URL.convertDate(max, new Date());
+		List<Post> list = service.fullSearch(text,minDate,maxDate);
 		
 		return ResponseEntity.ok().body(list);
 	}
