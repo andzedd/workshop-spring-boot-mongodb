a6da6cd5dbb9f3cefbd93e213af32c132bc4efa5
Matheus dos Santos Souza <ssouza.matheus@outlook.com>
2021-01-12 13:31:34 -0300

    getting user by id

--- src/main/java/com/andzedd/workshopmongo/resources/UserResource.java
+++ src/main/java/com/andzedd/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,12 @@ public class UserResource {
 		List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(usersDTO);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
