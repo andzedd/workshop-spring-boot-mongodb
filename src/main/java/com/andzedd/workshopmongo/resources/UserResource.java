1a06697d79c5fde58e10cc0c1c8a33a2540c56a1
Matheus dos Santos Souza <ssouza.matheus@outlook.com>
2021-01-12 15:45:03 -0300

    user posts endpoint

--- src/main/java/com/andzedd/workshopmongo/resources/UserResource.java
+++ src/main/java/com/andzedd/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.andzedd.workshopmongo.domain.Post;
 import com.andzedd.workshopmongo.domain.User;
 import com.andzedd.workshopmongo.dto.UserDTO;
 import com.andzedd.workshopmongo.services.UserService;
@@ -66,5 +67,12 @@ public class UserResource {
 		obj.setId(id);		
 		obj = service.update(obj);		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
